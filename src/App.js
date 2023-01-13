a90625648253d62a5c01a707007980d81305796a
Ana Gonzalez Hiraldo <gonzaana35@gmail.com>
2023-01-13 12:08:27 -0500

    We started Routes demo, imported BrowserRouter, Routes and Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react'
 
 function App() {
     return (
-        <>
-            <h1>hello, world</h1>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Router></Router>
+                <Router></Router>
+                <Router></Router>
+            </Routes>
+        </BrowserRouter>
+
+            
+        
     );
 }
 
